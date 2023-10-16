d03208b08b8e4617042692684465646a92db11de
jmorgainz <joshua.morgan.elite@gmail.com>
2023-10-16 14:32:39 -0400

    Searchbar component scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Searchbar from './components/Searchbar'
 
 function App() {
+
+   const handleSearch = (term: string, location: string) => {
+    console.log(`Searching for ${term} in ${location}`) 
+    // Make our api requests here
+   }
+
   return (
-    <h1>Hello World</h1>
+    <div className="App">
+      <h1>Hello World</h1>
+      <Searchbar onSearch={handleSearch}/>
+    </div>
   );
 }
 
