8695d930e422749de777d706d7e9b3d4f2ae0b60
jlamar27 <jlamar2712@gmail.com>
2023-10-20 17:31:39 -0400

    deploy netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
