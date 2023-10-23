a573021bd4197bbb9352e7e5a382e75f5617dad8
jmorgainz <joshua.morgan.elite@gmail.com>
2023-10-23 08:48:40 -0400

    some style changes.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import SearchBar from './Searchbar'; // Please ensure this import is correct based on your file structure.
-import api from '../api/apiConfig'; // Adjust if your import is different.
-import yelp from '../http/httpConfig'; // Adjust if your import is different.
-import { useCookies } from 'react-cookie';
+import SearchBar from './Searchbar'; 
+import api, { getCsrfToken } from '../api/apiConfig'; 
+import ResultsPage from './ResultsPage';
+import yelp from '../api/apiConfig';
+import axios from 'axios';
 
 const Navbar: React.FC = () => {
   const navigate = useNavigate();
-  const [cookie, setCookie, removeCookie] = useCookies(['csrftoken']);
+  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const isLoggedIn = () => {
+    const csrfToken = localStorage.getItem('csrf_token');
+    return csrfToken != null;
+  };
+
+  useEffect(() => {
+    const initializeAuth = async () => {
+      await getCsrfToken(); // Fetch the CSRF token.
+      setIsAuthenticated(isLoggedIn()); // Check if the user is authenticated.
+    };
+
+    initializeAuth();
+  }, []);
 
   // Function to initiate a predefined search.
   const handlePredefinedSearch = async (searchTerm: string) => {
     try {
-      // This example uses a fixed location, adjust as necessary for your needs.
       const response = await yelp.get('/businesses/search', {
         params: {
           term: searchTerm,
@@ -22,62 +37,49 @@ const Navbar: React.FC = () => {
         },
       });
 
-      // Redirecting to the results page with the obtained data.
       navigate('/results', { state: { data: response.data.businesses, term: searchTerm, location: 'New York City' } });
     } catch (error) {
       console.error('Error during predefined search:', error);
     }
   };
 
+  // Handle the logout process.
   const handleLogout = async (e: any) => {
     e.preventDefault();
-    console.log('Logging out...');
-
     try {
-      const csrfToken = localStorage.getItem('csrf_token'); 
-      console.log('This is the current CSRF Token Value', csrfToken)
-
       const response = await api.post('/logout/', {}, {
         headers: {
-          'X-CSRFToken': csrfToken
+          'Content-Type': 'application/json',
         }
       });
 
-      console.log("This is the response:", response.data)
-
       if (response.status === 200) {
-        // Redirect to the login or home page after logout
-        window.location.href = '/auth/signin';
+        localStorage.removeItem('csrf_token'); // Clear the token.
+        setIsAuthenticated(false); // Update auth state.
+        navigate('/auth/signin'); // Redirect to signin.
       }
     } catch (error) {
       console.error('Error logging out:', error);
     }
   };
 
+  // Render the component.
   return (
     <nav className="navbar">
       <div className="navbar-main">
-        <Link to="/" className="navbar-link home-link">
-          Home
-        </Link>
+        <Link to="/" className="navbar-link home-link">Home</Link>
         <SearchBar />
-        <Link to="/auth/signin" className="navbar-link signin-link">
-          Sign In
-        </Link>
+        {isAuthenticated ? (
+          <Link to="#" onClick={handleLogout} className="navbar-link logout-link">Logout</Link>
+        ) : (
+          <Link to="/auth/signin" className="navbar-link signin-link">Sign In</Link>
+        )}
       </div>
       <div className="navbar-links">
-        <div role="link" className="navbar-link" onClick={() => handlePredefinedSearch('restaurants')}>
-          Restaurants
-        </div>
-        <div role="link" className="navbar-link" onClick={() => handlePredefinedSearch('bars')}>
-          Bars
-        </div>
-        <div role="link" className="navbar-link" onClick={() => handlePredefinedSearch('shopping')}>
-          Shopping
-        </div>
-        <Link to="#" onClick={handleLogout} className="navbar-link logout-link">
-          Logout
-        </Link>
+        {/* Predefined search categories. */}
+        <div role="link" className="navbar-link" onClick={() => handlePredefinedSearch('restaurants')}>Restaurants</div>
+        <div role="link" className="navbar-link" onClick={() => handlePredefinedSearch('bars')}>Bars</div>
+        <div role="link" className="navbar-link" onClick={() => handlePredefinedSearch('shopping')}>Shopping</div>
       </div>
     </nav>
   );
--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Make sure 'react-router-dom' is version 6.x.x or above
-import api, { getCookie } from '../api/apiConfig'; // Adjust the import path if necessary
+import { useNavigate } from 'react-router-dom';
+import api, { getCookie } from '../api/apiConfig'; 
 
 export default function Signin() {
     const [username, setUsername] = useState<string>('');
@@ -15,11 +15,11 @@ export default function Signin() {
     
             if (response.status === 200) {
                 console.log('Backend response:', response.data);
-                console.log('CSRF Token on Login:', response.data.csrf_token);
                 localStorage.setItem('csrf_token', response.data.csrf_token);
-                console.log('SessionID:', response.data.sessionid)
+                // Redirecting to the home page after successful login
+                navigate('/'); 
             } else {
-                console.log('Failed to authenticate');
+                console.error('Failed to authenticate');
             }
         } catch (err) {
             console.error('An error occurred while trying to authenticate:', err);
@@ -31,30 +31,28 @@ export default function Signin() {
     };
 
     return (
-        <div>
-            <div className='signin-container'>
-                <h1 className='signin-title'>Sign In</h1>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        placeholder='Username'
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
-                        required
-                    />
-                    <input
-                        type="password"
-                        placeholder='Password'
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        required
-                    />
-                    <button type='submit' className='signin-button'>Sign In</button>
-                </form>
-                <p>
-                    Don't have an account? <span className='signup-link' onClick={handleSignupLink} style={{ color: 'blue', cursor: 'pointer' }}>Sign up here.</span>
-                </p>
-            </div>
+        <div className='signin-container'>
+            <h1 className='signin-title'>Sign In</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder='Username'
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder='Password'
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                />
+                <button type='submit' className='signin-button'>Sign In</button>
+            </form>
+            <p>
+                Don't have an account? <span className='signup-link' onClick={handleSignupLink}>Sign up here.</span>
+            </p>
         </div>
     );
 }
